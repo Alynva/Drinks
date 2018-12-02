b76190cf0737161613a89a2a3f93d78a09343217
Alisson Nunes <alynva@gmail.com>
2018-12-02 17:23:30 -0200

    Resolvido fatal error signal 6

--- app/src/main/java/com/alynva/drinks/drinks/scenarios_splash/SplashActivity.kt
+++ app/src/main/java/com/alynva/drinks/drinks/scenarios_splash/SplashActivity.kt
@@ -14,19 +14,19 @@ class SplashActivity : AppCompatActivity(), SplashContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-//        GlideApp.with(this)
-//                .load("https://us.123rf.com/450wm/nuttapol123rf/nuttapol123rf1503/nuttapol123rf150300262/38009601-table-top-and-blur-nature-of-background.jpg?ver=6")
-//                .centerCrop()
-//                .into(iv_splash_bg)
-//        tv_splash_bg_text.setText("Table Top And Blur Nature of Background\nby Nuttapol Yupothong")
-//
-//        GlideApp.with(this)
-//                .load("https://orig00.deviantart.net/0558/f/2018/168/5/2/_closed__summer_drink_ych_by_uszatyarbuz-dcagjie.gif")
-//                .into(iv_splash_gif)
-//        tv_splash_gif_text.setText("Summer drink YCH I\nby UszatyArbuz")
-//
-//        val presenter : SplashContract.Presenter = SplashPresenter(this)
-//        presenter.onLoadList()
+        GlideApp.with(this)
+                .load("https://us.123rf.com/450wm/nuttapol123rf/nuttapol123rf1503/nuttapol123rf150300262/38009601-table-top-and-blur-nature-of-background.jpg?ver=6")
+                .centerCrop()
+                .into(iv_splash_bg)
+        tv_splash_bg_text.setText("Table Top And Blur Nature of Background\nby Nuttapol Yupothong")
+
+        GlideApp.with(this)
+                .load("https://orig00.deviantart.net/0558/f/2018/168/5/2/_closed__summer_drink_ych_by_uszatyarbuz-dcagjie.gif")
+                .into(iv_splash_gif)
+        tv_splash_gif_text.setText("Summer drink YCH I\nby UszatyArbuz")
+
+        val presenter : SplashContract.Presenter = SplashPresenter(this)
+        presenter.onLoadList()
     }
 
     override fun saveList(drinks: List<Drink>) {
